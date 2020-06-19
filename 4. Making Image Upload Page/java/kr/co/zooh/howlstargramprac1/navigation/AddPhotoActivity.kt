dfe6eec6fe4da64a499086fc2338b6f0880c0652
Ju Hyeok <40654227+zero5two4@users.noreply.github.com>
2020-06-20 07:42:03 +0900

    Add files via upload

---
+++ 4. Making Image Upload Page/java/kr/co/zooh/howlstargramprac1/navigation/AddPhotoActivity.kt
@@ -0,0 +1,62 @@
+package kr.co.zooh.howlstargramprac1.navigation
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.storage.FirebaseStorage
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import kr.co.zooh.howlstargramprac1.R
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity : AppCompatActivity() {
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage : FirebaseStorage? = null
+    var photoUri : Uri? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        //Initiate storage
+        storage = FirebaseStorage.getInstance()
+
+        // Open the album
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        // add image upload event
+        addphoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_FROM_ALBUM){
+            if(resultCode == Activity.RESULT_OK){
+                // This is path to the selected image
+                photoUri = data?.data
+                addphoto_image.setImageURI(photoUri)
+            }else{
+                // Exit the addPhotoActivity if you leave the album without selecting it
+                finish()
+            }
+        }
+    }
+    fun contentUpload(){
+        // Make filename
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.PNG"
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        //FileUpload
+        storageRef?.putFile(photoUri!!)?.addOnSuccessListener {
+            Toast.makeText(this, getString(R.string.upload_success), Toast.LENGTH_LONG).show()
+        }
+    }
+}
