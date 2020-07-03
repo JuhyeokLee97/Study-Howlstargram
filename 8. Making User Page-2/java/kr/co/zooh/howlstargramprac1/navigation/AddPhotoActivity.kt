3683a0233d7705a006446aeb28073a998d01205c
Ju Hyeok <40654227+zero5two4@users.noreply.github.com>
2020-07-04 02:27:53 +0900

    Add files via upload

---
+++ 8. Making User Page-2/java/kr/co/zooh/howlstargramprac1/navigation/AddPhotoActivity.kt
@@ -0,0 +1,93 @@
+package kr.co.zooh.howlstargramprac1.navigation
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
+import kotlinx.android.synthetic.main.activity_add_photo.*
+import kr.co.zooh.howlstargramprac1.R
+import kr.co.zooh.howlstargramprac1.navigation.model.ContentDTO
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddPhotoActivity : AppCompatActivity() {
+    var PICK_IMAGE_FROM_ALBUM = 0
+    var storage: FirebaseStorage? = null
+    var photoUri: Uri? = null
+    var auth: FirebaseAuth? = null
+    var firestore: FirebaseFirestore? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+
+        //Initiate
+        storage = FirebaseStorage.getInstance()
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+        // Open the album
+        var photoPickerIntent = Intent(Intent.ACTION_PICK)
+        photoPickerIntent.type = "image/*"
+        startActivityForResult(photoPickerIntent, PICK_IMAGE_FROM_ALBUM)
+
+        // add image upload event
+        addphoto_btn_upload.setOnClickListener {
+            contentUpload()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == PICK_IMAGE_FROM_ALBUM) {
+            if (resultCode == Activity.RESULT_OK) {
+                // This is path to the selected image
+                photoUri = data?.data
+                addphoto_image.setImageURI(photoUri)
+            } else {
+                // Exit the addPhotoActivity if you leave the album without selecting it
+                finish()
+            }
+        }
+    }
+
+    fun contentUpload() {
+        // Make filename
+        var timestamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        var imageFileName = "IMAGE_" + timestamp + "_.PNG"
+
+        var storageRef = storage?.reference?.child("images")?.child(imageFileName)
+
+        // Promise method
+        storageRef?.putFile(photoUri!!)?.continueWithTask { task : Task<UploadTask.TaskSnapshot>->
+            return@continueWithTask storageRef.downloadUrl
+        }?.addOnSuccessListener { uri ->
+            var contentDTO = ContentDTO()
+
+            // Insert downloadUrl of image
+            contentDTO.imageUri = uri.toString()
+
+            // Insert uid of user
+            contentDTO.uid = auth?.currentUser?.uid
+
+            // Insert userId
+            contentDTO.userId = auth?.currentUser?.email
+
+            // Insert explain of content
+            contentDTO.explain = addphoto_edit_explain.text.toString()
+
+            firestore?.collection("images")?.document()?.set(contentDTO)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
+
+        }
+    }
+}
