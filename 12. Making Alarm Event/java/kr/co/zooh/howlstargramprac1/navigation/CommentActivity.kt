3885fcab0ff75628d177e891afcf7380b1a823f3
Ju Hyeok <40654227+zero5two4@users.noreply.github.com>
2020-07-07 07:12:21 +0900

    Add files via upload

---
+++ 12. Making Alarm Event/java/kr/co/zooh/howlstargramprac1/navigation/CommentActivity.kt
@@ -0,0 +1,102 @@
+package kr.co.zooh.howlstargramprac1.navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_comment.*
+import kotlinx.android.synthetic.main.item_comment.view.*
+import kr.co.zooh.howlstargramprac1.R
+import kr.co.zooh.howlstargramprac1.navigation.model.AlarmDTO
+import kr.co.zooh.howlstargramprac1.navigation.model.ContentDTO
+
+class CommentActivity : AppCompatActivity() {
+    var contentUid : String? = null
+    var destinationUid : String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment)
+        contentUid = intent.getStringExtra("contentUid")
+        destinationUid = intent.getStringExtra("destinationUid")
+
+        comment_recyclerview.adapter = CommentRecyclerviewAdapter()
+        comment_recyclerview.layoutManager = LinearLayoutManager(this)
+
+        comment_btn_send?.setOnClickListener {
+            var comment = ContentDTO.Comment()
+            comment.userId = FirebaseAuth.getInstance().currentUser?.email
+            comment.uid = FirebaseAuth.getInstance().currentUser?.uid
+            comment.comment = comment_edit_message.text.toString()
+            comment.timestamp = System.currentTimeMillis()
+
+            FirebaseFirestore.getInstance().collection("images").document(contentUid!!).collection("comments").document().set(comment)
+            commentAlarm(destinationUid!!, comment_edit_message.text.toString())
+            comment_edit_message.setText("")
+        }
+    }
+
+    fun commentAlarm(destinationUid : String, message : String){
+        var alarmDTO = AlarmDTO()
+        alarmDTO.destinationUid = destinationUid
+        alarmDTO.userId = FirebaseAuth.getInstance().currentUser?.email
+        alarmDTO.uid = FirebaseAuth.getInstance().currentUser?.uid
+        alarmDTO.timestamp = System.currentTimeMillis()
+        alarmDTO.message = message
+        FirebaseFirestore.getInstance().collection("alarms").document().set(alarmDTO)
+    }
+    inner class CommentRecyclerviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+        var comments : ArrayList<ContentDTO.Comment> = arrayListOf()
+        init{
+            FirebaseFirestore.getInstance()
+                .collection("images")
+                .document(contentUid!!)
+                .collection("comments")
+                .orderBy("timestamp")
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    comments.clear()
+                    if (querySnapshot == null) return@addSnapshotListener
+
+                    for (snapshot in querySnapshot.documents!!){
+                        comments.add(snapshot.toObject(ContentDTO.Comment::class.java)!!)
+                    }
+                    notifyDataSetChanged()
+                }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            var view = LayoutInflater.from(parent.context).inflate(R.layout.item_comment, parent, false)
+            return CustomViewHolder(view)
+        }
+
+        inner class CustomViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+        override fun getItemCount(): Int {
+            return comments.size
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            var view = holder.itemView
+            view.commentview_textview_comment.text = comments[position].comment
+            view.commentview_textview_profile.text = comments[position].userId
+
+            FirebaseFirestore.getInstance()
+                .collection("profileImages")
+                .document(comments[position].uid!!)
+                .get()
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful){
+                        var url = task.result!!["image"]
+                        Glide.with(holder.itemView.context).load(url).apply(RequestOptions().circleCrop()).into(view.commentview_imageview_profile)
+                    }
+                }
+        }
+
+    }
+}
---
+++ 12. Making Alarm Event/java/kr/co/zooh/howlstargramprac1/navigation/model/AlarmDTO.kt
@@ -0,0 +1,10 @@
+package kr.co.zooh.howlstargramprac1.navigation.model
+
+data class AlarmDTO(
+    var destinationUid : String? = null,
+    var userId : String? = null,
+    var uid : String? = null,
+    var kind : Int? = null,
+    var message : String? = null,
+    var timestamp : Long? = null
+)
